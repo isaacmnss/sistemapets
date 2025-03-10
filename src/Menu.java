d44340c9d239172d6b85a5dd6d927a25a0f1c7a4
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2025-03-10 14:45:58 -0300

    initial: first commit

---
+++ src/Menu.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Menu {
+    void executarMenu(){
+        Scanner sc = new Scanner(System.in);
+        int opc = 0;
+        while (opc != 7){
+            System.out.println("Digite um número");
+            opc = sc.nextInt();
+
+
+
+        }
+    }
+}
